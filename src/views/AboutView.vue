a0c6207cec98dc7442a223f34acd71378738c9e0
baaami <52270698+SeongBeom27@users.noreply.github.com>
2022-10-05 19:54:25 +0900

    create vue project

---
+++ src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>This is About</nav>
+</template>
+<script>
+export default {
+  name: ``,
+  components: {},
+  data() {
+    return {
+      sampleData: ``,
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {},
+}
+</script>
