4cbc9c6be8b29c7cb98d60e5b541b290aec60a93
baaami <52270698+SeongBeom27@users.noreply.github.com>
2022-10-07 16:40:02 +0900

    [DATA BINDING] 리스트 렌더링

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import DataBinding from '../views/DataBindingForm.vue'
+import DataBinding from '../views/DataBindingList.vue'
 
 const routes = [
   {
